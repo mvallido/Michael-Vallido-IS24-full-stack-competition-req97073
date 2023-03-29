82428e2149ed205a354fa5be158ec004f17e5739
mvallido <vallido.michael@gmail.com>
2023-03-28 22:57:38 -0700

    Removing DB config
    Commenting out the DB config and just using In memory data storage. The DB connection does work, but I'll stick to In memory data storage as per the competition requirements.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,30 +1,45 @@
 const express = require('express');
+const { faker } = require('@faker-js/faker');
 const { sequelize } = require('../models/index')
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-const connectDb = async () => {
-  console.log('Checking database connection...')
+let db = [];
 
-  try {
-    await sequelize.authenticate()
-    console.log('Connection to Postgres has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-}
+// Middleware to parse JSON request bodies
+app.use(express.json());
 
-(async () => {
-  await connectDb()
+app.get('/health', (req, res) => {
+  res.status(200).send('API component is healthy');
+});
 
-  app.get('/', (req, res) => {
-    res.send("Root");
-  });
+// Route to get all users
+app.get('/api/test', (req, res) => {
+  res.json(db);
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+// const connectDb = async () => {
+//   console.log('Checking database connection...')
+
+//   try {
+//     await sequelize.authenticate()
+//     console.log('Connection to Postgres has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// }
+
+// (async () => {
+//   await connectDb()
+
+//   app.get('/', (req, res) => {
+//     res.send("Root");
+//   });
   
-  app.use('/api/test', require('../routes/users'));
+//   app.use('/api/test', require('../routes/users'));
   
-  app.listen(port, () => console.log(`Server started on port ${port}`));
-})();
-
-
+//   app.listen(port, () => console.log(`Server started on port ${port}`));
+// })();
